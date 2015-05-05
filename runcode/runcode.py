aa912a1303460bed35537d04b027fee846c0697f
HackToday <wkq5325@gmail.com>
2015-05-05 05:19:46 +0000

    Add ruby code run support

--- runcode/runcode.py
+++ runcode/runcode.py
@@ -63,3 +63,30 @@ class RunPyCode(object):
             f.write(code)
         self._run_py_prog(filename)
         return self.stderr, self.stdout
+
+
+class RunRubyCode(object):
+
+    def __init__(self, code=None):
+        self.code = code
+        if not os.path.exists('running'):
+            os.mkdir('running')
+
+        self.compiler = "ruby"
+
+    def _run_rb_prog(self, cmd="a.rb"):
+        cmd = [self.compiler, cmd]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = p.wait()
+        a, b = p.communicate()
+        self.stdout, self.stderr = a.decode("utf-8"), b.decode("utf-8")
+        return result
+
+    def run_rb_code(self, code=None):
+        filename = "./running/a.rb"
+        if not code:
+            code = self.code
+        with open(filename, "w") as f:
+            f.write(code)
+        self._run_rb_prog(filename)
+        return self.stderr, self.stdout
