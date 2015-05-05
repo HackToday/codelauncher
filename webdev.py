879a10e37362218a61cdb6bae259b58c26ef2ba6
HackToday <wkq5325@gmail.com>
2015-05-05 05:30:05 +0000

    Add template change and support override host,port

--- webdev.py
+++ webdev.py
@@ -1,3 +1,4 @@
+import argparse
 from flask import Flask, render_template, request
 from runcode import runcode
 app = Flask(__name__)
@@ -18,6 +19,13 @@ if __name__ == "__main__":
     print "Hello Python World!!"
 """
 
+default_ruby_code = """
+
+if __FILE__ == $0
+    puts "Hello Ruby World!"
+end
+
+"""
 default_rows = "15"
 default_cols = "60"
 
@@ -62,5 +70,30 @@ def runpy():
                            rescomp=rescompil,#"No compilation for Python",
                            rows=default_rows, cols=default_cols)
 
+@app.route("/rb")
+@app.route("/runrb", methods=['POST', 'GET'])
+def runrb():
+    if request.method == 'POST':
+        code = request.form['code']
+        run = runcode.RunRubyCode(code)
+        rescompil, resrun = run.run_rb_code()
+        if not resrun:
+            resrun = 'No result!'
+    else:
+        code = default_ruby_code
+        resrun = 'No result!'
+        rescompil = "No compilation for Ruby"
+
+    return render_template("main.html",
+                           code=code,
+                           target="runrb",
+                           resrun=resrun,
+                           rescomp=rescompil,#"No compilation for Python",
+                           rows=default_rows, cols=default_cols)
+
 if __name__ == "__main__":
-    app.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='0.0.0.0', help='Web Server Listen Host')
+    parser.add_argument('--port', default=5003, help='Web Server Running Port')
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port)
